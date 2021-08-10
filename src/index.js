30b23dc2835930202bb9f740823f9d6222aa2d6b
OJP98 <ojuarez_p@hotmail.com>
2021-08-10 03:39:34 -0600

    Editor resizable and some shortcuts

--- src/index.js
+++ src/index.js
@@ -10,43 +10,53 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/c_cpp");
 editor.setKeyboardHandler("ace/keyboard/vim");
+editor.setAutoScrollEditorIntoView(true);
+editor.commands.addCommand({
+  name: "parseFile",
+  bindKey: {win: "Shift-enter", mac: "Shift-enter"},
+  exec: () => main()
+});
+editor.execCommand("showKeyboardShortcuts")
 
-console.log(editor.getValue());
 
-var errors = [];
+document.getElementById("parseButton").addEventListener("click", main);
 
-const input = editor.getValue();
+function main() {
+  console.clear();
+  var errors = [];
+  const input = editor.getValue();
 
-const chars = new antlr4.InputStream(input);
-const lexer = new DecafLexer(chars);
-const tokens = new antlr4.CommonTokenStream(lexer);
-const parser = new DecafParser(tokens);
-parser.buildParseTrees = true;
+  const chars = new antlr4.InputStream(input);
+  const lexer = new DecafLexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new DecafParser(tokens);
+  parser.buildParseTrees = true;
 
-console.log(parser.ruleNames);
+  console.log(parser.ruleNames);
 
-const tree = parser.program();
-const decafVisitor = new DecafVisitor();
-tree.accept(decafVisitor);
+  const tree = parser.program();
+  const decafVisitor = new DecafVisitor();
+  tree.accept(decafVisitor);
 
-// Main program
-const mainMethod = decafVisitor.methods.find((m) => m.name == 'main');
+  // Main program
+  const mainMethod = decafVisitor.methods.find((m) => m.name == 'main');
 
-if (!mainMethod)
-  errors.push(new MainNotDefinedError().ErrorLog);
+  if (!mainMethod)
+    errors.push(new MainNotDefinedError().ErrorLog);
 
-const symbols = decafVisitor.symbols;
-const methods = decafVisitor.methods;
+  const symbols = decafVisitor.symbols;
+  const methods = decafVisitor.methods;
 
-console.table(methods);
-console.table(symbols);
+  console.table(methods);
+  console.table(symbols);
 
-// agregamos los errores al arreglo de errores
-methods.forEach((m) => m.error ? errors.push(m.Error) : '');
-symbols.forEach((s) => s.error ? errors.push(s.Error) : '');
+  // agregamos los errores al arreglo de errores
+  methods.forEach((m) => m.error ? errors.push(m.Error) : '');
+  symbols.forEach((s) => s.error ? errors.push(s.Error) : '');
 
-if (errors.length)
-  errors.forEach((err) => console.error(err));
-else
-  console.info('No errors found!');
+  if (errors.length)
+    errors.forEach((err) => console.error(err));
+  else
+    console.info('No errors found!');
 
+}
